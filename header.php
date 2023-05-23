40b478366a3b05881fc1e09a0b6760d1f15de21a
welltohell <el1xz@list.ru>
2023-05-23 15:33:19 +0300

    add favicon

--- header.php
+++ header.php
@@ -8,6 +8,10 @@ session_start();
 	<meta charset="utf-8">
 	<meta name=viewport content="width=device-width, initial-scale=1">
 	<title>СИНТЕЗ</title>
+	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
 	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
 		integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
