584c52df54d32b572abf3b796e928626bc8ad98a
welltohell <el1xz@list.ru>
2023-05-23 15:34:12 +0300

    update title

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<meta name=viewport content="width=device-width, initial-scale=1">
-	<title>СИНТЕЗ</title>
+	<title>Синтез</title>
 	<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
