eadb8257fb25d23bf50b0c1ca334025b3a611310
welltohell <el1xz@list.ru>
2023-05-29 19:15:36 +0300

    add btns close functions

--- header.php
+++ header.php
@@ -39,7 +39,7 @@ session_start();
 				<!-- Modal content -->
 				<div x-show="login" x-transition @click.away="login = false"
 					class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-					<button type="button"
+					<button @click="login = false" type="button"
 						class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
 						<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
 							xmlns="http://www.w3.org/2000/svg">
@@ -86,7 +86,7 @@ session_start();
 				<!-- Modal content -->
 				<div x-show="register" x-transition @click.away="register = false"
 					class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-					<button type="button"
+					<button @click="register = false" type="button"
 						class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
 						<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
 							xmlns="http://www.w3.org/2000/svg">
